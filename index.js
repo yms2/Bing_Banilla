13f617ecd1f522a9dd6a884ea8c1ced700585b48
Yu Min Seung <90673332+yms2@users.noreply.github.com>
2022-08-19 16:09:20 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ lang.addEventListener('click', () =>{
 langClose.addEventListener('click', () => {
     langBox.style.display = 'none';
 });
+
+
+let header = document.querySelector(".header_inner");
+let headerHeight = header.offsetHeight;
+
+window.onscroll = function() {
+    let windowTop = window.scrollY;
+
+    if(windowTop >= headerHeight){
+        header.classList.add("drop");
+    }else{
+        header.classList.remove("drop")
+    }
+}
