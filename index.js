f4d299940bde5f95b9cede041bc137e910be1921
DESKTOP-R7TQ6PG\dbals <dbalstjd789@daum.net>
2022-12-26 10:23:03 +0900

    12.26 헤더탑 수정

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const lang = document.querySelector('.lang > a');
 const langBox =  document.querySelector('.lang_box');
-const langClose = langBox.querySelector('.lang_close');
 
 lang.addEventListener('click', () =>{
     langBox.style.display = 'block';
